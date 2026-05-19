0dc5136aa7e4a59b1a465560db1dbe9c6e539a21
pablomg-dev <pablomg.developer@gmail.com>
2026-05-19 19:44:11 -0300

    feat: integrar Google Analytics (GA4) con @astrojs/partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   site: "https://pablomgdev.lat",
   devToolbar: {
     enabled: false,
